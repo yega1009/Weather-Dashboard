e46b81de76647234613c9d228f503f1ca3a03192
Yega Cho <yega1009@gmail.com>
2023-08-15 13:52:32 -0400

    declared variables for dom elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var apiKey = "10b49d2406d4cabc310ecd5ccbb9da7f";
+var cityName = document.getElementById("city-name");
+var searchBtn = document.getElementById("search-btn");
+var searchHistory = document.getElementById("search-history");
+var currentWeather = document.getElementById("current-weather");
+var futureWeather = document.getElementById("future-weather");
