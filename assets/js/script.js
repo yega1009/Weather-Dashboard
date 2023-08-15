6db97a803eb9212b20f52147b41fef031e9009ee
Yega Cho <yega1009@gmail.com>
2023-08-15 14:32:25 -0400

    created function to fetch and display weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,43 @@ var searchHistory = document.getElementById("search-history");
 var currentWeather = document.getElementById("current-weather");
 var futureWeather = document.getElementById("future-weather");
 
-searchBtn.on('click', function (event) {
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var city = cityName.value;
     if (city) {
         getWeather(city);
     }
 });
+
+function getWeather(city) {
+    var currentApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+    var futureApi = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+
+    fetch(currentApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayCurrentWeather(data);
+                fetch(futureApi).then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            displayFutureWeather(data);
+                            addCityToHistory(city);
+                        });
+                    } else {
+                        alert('Failed to fetch future weather data' + response.statusText);
+                    }
+                });
+            });
+        } else {
+            alert('Failed to fetch current weather data' + response.statusText);
+        }
+    }).catch(function(error) {
+        alert('Error during fetch');
+    });
+};
+
+function displayCurrentWeather(data) 
+
+function displayFutureWeather(data) 
+
+function addCityToHistory(city) 
