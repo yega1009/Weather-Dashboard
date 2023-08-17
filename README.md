04cafd3ae29fc190c3561ddcb2c49db89a115731
Yega Cho <yega1009@gmail.com>
2023-08-17 17:47:42 -0400

    added comments, updated readme file, added display search history function

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Weather-Dashboard
+
+This is an app that enables users to view the current weather and the 5-day forecast of a chosen city.
+
+## Link to Deployed Application
+<a href="https://yega1009.github.io/Weather-Dashboard/">Weather Dashboard</a>
+
+## Description
+- This web application is designed to provide current weather conditions and a 5-day forecast for a chosen city by the user.
+- The application fetches data from the OpenWeatherMap API and presents it to the user.  
+- The dashboard consists of weather description such as weather icons, temperature, wind speed, and humidity. 
+- It also allows users to revisit their recent searches.
+
+## Installation
+
+N/A
+
+## Usage
+- Users can enter the city name in the input field, click the "Search" button, and the current weather 
+for the selected city will show on the right, along with a 5-day forecast below it.
+- Users can view recent search history by clicking on the city name form the list of cities they previously searched for.
+- Users can view the weather icon that represents the current condition, temperature, wind speed, and humidity of the city.
+
+## Screenshot of the Website
+
+![Screenshot 1](./assets/images/Screenshot-1.png)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+// Define API key for OpenWeatherMap
 var apiKey = "10b49d2406d4cabc310ecd5ccbb9da7f";
+// Get HTML elements by their IDs
 var cityName = document.getElementById("city-name");
 var searchBtn = document.getElementById("search-btn");
 var clearHistoryBtn = document.getElementById("clear-history");
@@ -6,6 +8,7 @@ var searchHistory = document.getElementById("search-history");
 var currentWeather = document.getElementById("current-weather");
 var futureWeather = document.getElementById("future-weather");
 
+// Add a click event listener to the search button
 searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var city = cityName.value;
@@ -14,20 +17,27 @@ searchBtn.addEventListener('click', function (event) {
     }
 });
 
-clearHistoryBtn.addEventListener('click', function() {
+// Add a click event listener to clear search history button
+clearHistoryBtn.addEventListener('click', function () {
     localStorage.removeItem("cities");
     searchHistory.innerHTML = '';
 });
 
-
+// Fetch and display weather details for the given city
 function getWeather(city) {
+    if (!city) {
+        return;
+    }
+    // API endpoints
     var currentApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
     var futureApi = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
 
+    // Fetch current weather data
     fetch(currentApi).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayCurrentWeather(data);
+                // Fetch future weather data
                 fetch(futureApi).then(function (response) {
                     if (response.ok) {
                         response.json().then(function (data) {
@@ -47,16 +57,17 @@ function getWeather(city) {
     });
 };
 
+// Display current weather details
 function displayCurrentWeather(data) {
     var currentDate = new Date();
     var date = currentDate.toLocaleDateString();
-    var headerText = "<h2>" + data.name + " (" + date + ")</h2>";
 
+    // HTML strings for displaying current weather details
+    var headerText = "<h2>" + data.name + " (" + date + ")</h2>";
     var iconCode = data.weather[0].icon;
     var weatherDescription = data.weather[0].description;
     var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
     var weatherIcon = '<img src="' + iconURL + '" alt="' + weatherDescription + '">';
-
     var temperatureText = "<p>Temp: " + data.main.temp + "°K</p>";
     var windSpeedText = "<p>Wind: " + data.wind.speed + " m/s</p>";
     var humidityText = "<p>Humidity: " + data.main.humidity + "%</p>";
@@ -65,18 +76,19 @@ function displayCurrentWeather(data) {
     currentWeather.style.display = "block";
 }
 
+// Display 5-day forecast details
 function displayFutureWeather(data) {
     var forecast = "";
     for (var i = 3; i < data.list.length; i += 8) {
         var day = data.list[i];
         var date = new Date(day.dt * 1000).toLocaleDateString();
 
+        // HTML strings for displaying each day's forecast
         var headerText = "<h3>" + date + "</h3>";
         var weatherIcon = '<img src="https://openweathermap.org/img/w/' + day.weather[0].icon + '.png" alt="' + day.weather[0].description + '">';
         var temperatureText = "<p>Temp: " + day.main.temp + "°K</p>";
         var windSpeedText = "<p>Wind: " + day.wind.speed + " m/s</p>";
         var humidityText = "<p>Humidity: " + day.main.humidity + "%</p>";
-
         forecast += "<div>";
         forecast += headerText;
         forecast += weatherIcon;
@@ -89,8 +101,10 @@ function displayFutureWeather(data) {
     futureWeather.style.display = "flex";
 }
 
+// Add the city to the search history and store it in local storage
 function addCityToHistory(city) {
     var cities = JSON.parse(localStorage.getItem("cities")) || [];
+    // Add city to local storage if it doesn't already exist in the history
     if (!cities.includes(city)) {
         cities.push(city);
         localStorage.setItem("cities", JSON.stringify(cities));
@@ -98,7 +112,23 @@ function addCityToHistory(city) {
     displaySearchHistory();
 }
 
+// Display search history from local storage
 function displaySearchHistory() {
+    var cities = JSON.parse(localStorage.getItem("cities")) || [];
+    console.log(cities);
+    searchHistory.innerHTML = '';
+    // Loop through each city in the history
+    for (var i = 0; i < cities.length; i++) {
+        var cityName = cities[i];
+        // Create a button for each city in history
+        var cityBtn = document.createElement("button");
+        cityBtn.textContent = cityName;
+        // Attach a click event to each city button to fetch its weather when clicked
+        cityBtn.addEventListener('click', function (event) {
+            getWeather(event.target.textContent);
+        })
+        searchHistory.appendChild(cityBtn);
+    }
 }
 
 displaySearchHistory()
